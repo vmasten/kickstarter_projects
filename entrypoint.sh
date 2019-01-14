4c03fbf237870e49bc5f539f71b02d9b46733b0c
Vince Masten <vmasten@me.com>
2019-01-14 15:34:48 -0800

    switching drivers

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+set -e
+
+cd /src
+
+# coverage run manage.py test -v 2 && \
+# coverage report
+# python3 manage.py makemigrations --noinput
+# python3 manage.py migrate --noinput
+python3 manage.py runserver 0.0.0.0:8000
