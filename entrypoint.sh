27ebd9d4be57e66b3ec1bf93d355310edeb245a3
Vince Masten <vmasten@me.com>
2019-01-16 13:45:19 -0800

    more practice

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput && \
 # python3 load_db.py
-python3 manage.py runserver 0.0.0.0:8000
+# python3 manage.py runserver 0.0.0.0:8000
